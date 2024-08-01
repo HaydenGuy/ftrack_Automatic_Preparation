0d8cf3bfe39b097aa8373ae353952ae3ae7c5a7b
hayden <haydenguyn@gmail.com>
2024-08-01 16:06:09 -0700

    Created create_task function and added it to the sequence/shot/task creation in list_files_recursively

--- ftrack_auto_prep.py
+++ ftrack_auto_prep.py
@@ -7,6 +7,8 @@ session = ftrack_api.Session(server_url="https://hguy.ftrackapp.com",
                              api_user="haydenguyn@hotmail.com",
                              api_key="Mzg5MzI0NzUtMTFlMC00YTEzLWEyZmItNzY4N2Q0YTEwZGZlOjowNmVkNTRiOC1lMGQwLTRiYjMtOTVmYi0zMDZlZDczNjBlMmY")
 
+# TODO Fix this function: change name, refactor some stuff into its own functions
+
 # Gets names of the directories in argv[1] folder and subfolders using recursion
 def list_files_recursively(directory_path, project, indent_level=0):
     with os.scandir(directory_path) as entries:
@@ -25,7 +27,11 @@ def list_files_recursively(directory_path, project, indent_level=0):
                         sequence_obj = session.query(f"Sequence where name is '{seq}'").one() # Query the sequence name and return its object on ftrack
                         shots = os.listdir(f"{entry.path}/{seq}") # Get list of shot dirs within a sequence dir
                         for shot in shots:
-                            create_shot(shot, sequence_obj) # Create the shot objets in ftrack
+                            create_shot(shot, sequence_obj) # Create the shot objects in ftrack
+                            shot_obj = session.query(f"Shot where name is {shot}").one() # Query the shot name and return its object on ftrack
+                            tasks = os.listdir(f"{entry.path}/{seq}/{shot}") # Get a list of the task dirs within the shot dir
+                            for task in tasks:
+                                create_task(task, task, shot_obj) # Create the task objects in ftrack
                 else:
                     list_files_recursively(entry.path, indent_level+1)
                 
@@ -115,6 +121,27 @@ def create_shot(name, parent):
     session.commit()
 
     return shot
+
+# Create a Task object
+def create_task(name, type, parent):
+     # Type ID's for each of the task types
+    options = {"Animation": "44dc3636-4164-11df-9218-0019bb4983d8",
+               "Rendering": "262225e8-9dcb-11e9-82b8-d27cf242b68b"
+    }
+    
+    task_type = options.get(type) # Gets the type ID for the passed type
+
+    task = session.create("Task", {
+        "name": name,
+        "parent": parent,
+        "type_id": task_type
+    })
+
+    # TODO Add error checking for duplicate entry
+
+    session.commit()
+
+    return task
 
 def main():
     # Print message and exit unless a single argument is given
